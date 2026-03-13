809e5fa2911b8591d95fed81a62531d7b0249424
Danh Vu <ledanhvu090888@gmail.com>
2026-03-13 21:11:44 +0700

    feature pagination

--- src/components/common/index.ts
+++ src/components/common/index.ts
@@ -31,3 +31,4 @@ export { default as TwoColumnImageGrid } from './TwoColumnImageGrid.vue';
 export { default as VClickOutside } from './v-click-outside.vue';
 export { default as YouTubeEmbed } from './YouTubeEmbed.vue';
 export { default as CommonGridShape } from './CommonGridShape.vue';
+export { default as Pagination } from './Pagination.vue';
